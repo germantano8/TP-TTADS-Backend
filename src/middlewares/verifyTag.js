ef8916e617756acde3ee405644ec81623db7cfb6
Germán Tano <germantano88@gmail.com>
2022-05-04 14:11:23 -0300

    creado todo lo necesario para categorías

--- src/middlewares/verifyTag.js
+++ src/middlewares/verifyTag.js
@@ -4,10 +4,10 @@ const verifyTag = (req,res,next) => {
     const {description} = req.body;
 
 
-    if(description && validator.isLength(description, {min: 4, max:undefined})) return next();
+    if(description && validator.isLength(description, {min: 4, max:200})) return next();
 
-    res.status(400).send({success:false, errors:{
-        description: "description must be at least 4 characters"
+    return res.status(400).send({success:false, errors:{
+        description: "description must be between 4 and 200 characters"
     }})
 
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ let express = require("express");
 let role = require("./role");
 let user = require("./user");
 let tag = require('./tag');
+let category = require('./category');
 
 let router = express.Router();
 
 router.use('/roles', role);
 router.use('/users', user);
 router.use("/tags", tag);
+router.use('/categories', category);
 
 module.exports = router;
