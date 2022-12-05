f4aad585dc89e55a60e0f328c1411f0b67f4f56a
Alejandro Navarini <anavarini@frro.utn.edu.ar>
2022-12-05 20:47:56 -0300

    Rename auths

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const controller = require('../controllers/order');
-const { verifyMongooseID, verifyOrder, auth } = require('../middlewares');
+const { verifyMongooseID, verifyOrder, authMiddleware } = require('../middlewares');
 
 const router = express.Router();
 
 router.get('/', controller.getOrders);
 router.get('/:id/', verifyMongooseID, controller.getOrder);
-router.post('/', auth, verifyOrder, controller.createOrder);
+router.post('/', authMiddleware, verifyOrder, controller.createOrder);
 router.delete('/:id/', verifyMongooseID, controller.deleteOrder);
 router.put('/:id/', verifyMongooseID, verifyOrder, controller.updateOrder);
 
